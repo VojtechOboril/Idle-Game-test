6093ab6bba77d09b7c5d2f60de5104ffe4ad8deb
Vojtech Oboril <vojtech.oboril@gmail.com>
2023-05-11 05:07:56 +0100

    part 3

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ var gameData = {
     gold: 0,
     goldPerClick: 1,
     goldPerClickCost: 10,
-    goldMiningSkill: new Skill("Gold Mining", 10, changeHTMLFunction = changeSkillMineGoldHTML)
+    goldMiningSkill: new Skill("Gold Mining", 10, 0, 0, function (x) { return x + 1 }, changeHTMLFunction = changeSkillMineGoldHTML)
 }
 
 function buyGoldPerClick() {
